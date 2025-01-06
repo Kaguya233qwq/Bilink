1bd36391b49778e35944aea3913c5c4d9a4624c7
marisa <1435608435@qq.com>
2025-01-06 19:49:53 +0800

    🐛fix: plugin import issue

--- src/bilink/core/plugin.py
+++ src/bilink/core/plugin.py
@@ -1,7 +1,6 @@
 import importlib
 from pathlib import Path
 from ..utils.logger import Logger
-from ..plugins import *
 
 
 def load_all():
@@ -10,14 +9,14 @@ def load_all():
     """
     # 获取指定目录路径下的所有 .py 文件
 
-    # directory = Path("plugins")
-    # if not directory.exists():
-    #     Logger.success("插件目录初始化成功，请在plugins文件夹下创建你的插件")
-    #     directory.mkdir()
-    #     return
-    # for module_file in directory.glob("*.py"):
-    #     # 获取模块名（去掉 .py 后缀）
-    #     module_name = module_file.stem
-    #     # 动态导入模块
-    #     importlib.import_module(f"plugins.{module_name}")
-    #     Logger.success(f"plugin {module_name} loaded successfully")
+    directory = Path(__file__).parent.parent / "plugins"
+    if not directory.exists():
+        Logger.success("插件目录初始化成功，请在plugins文件夹下创建你的插件")
+        directory.mkdir()
+        return
+    for module_file in directory.glob("*.py"):
+        # 获取模块名（去掉 .py 后缀）
+        module_name = module_file.stem
+        # 动态导入模块
+        importlib.import_module(f".plugins.{module_name}","src.bilink")
+        Logger.success(f"plugin {module_name} loaded successfully")
